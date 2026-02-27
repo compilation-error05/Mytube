3616f139babe910ef5283c5fad330cfe4af54e3d
Shantanu mukherjee <130738064+compilation-error05@users.noreply.github.com>
2026-02-28 00:18:45 +0530

    Initial backend setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,42 @@
 import {asyncHandler} from'../utils/asyncHandler.js';
+import {ApiError} from '../utils/Apierror.js';
+import{User} from '../models/user.model.js'
+import {uploadOnCloudinary} from '../utils/cloudinary.js';
+import {ApiResponse} from '../utils/apiResponse.js';
 const registerUser=asyncHandler(async(req,res)=>{
-    res.status(200).json(
-        {
-            message:"okay"
-        }
+    
+    const {fullName,username,email,password}=req.body;
+    if([fullName,username,email,password].some((field)=>field?.trim()===""))
+    {
+        throw new ApiError(400,"All fields are required");
+    }
+    const existedUser=User.findOne({
+        $or:[{username},{email}]
+    })
+    if(existedUser){
+        throw new ApiError(409,"Username or email already exists")
+    }
+    const avatarLocalPath=req.files?.avatar[0]?.path
+    const coverImageLocalPath=req.files?.coverImage[0]?.path
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar is required")
+    }
+    const avatar=await uploadOnCloudinary(avatarLocalPath)
+    const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+    if(!avatar){
+        throw new ApiError(400,"Failed to upload avatar")
+    }
+    User.create({
+        fullName,
+        avatar:avatar.url,
+        coverImage:coverImage?.url||"",
+        email,
+        password,
+        username:username.toLowerCase()
+    })
+    const createdUser=await User.findById(user._id).select("-password -refreshToken")
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully")
     )
-})
-export{registerUser}
+});
+export {registerUser}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,86 +1,100 @@
-import mongoose,{Schema} from "mongoose";
-import {JsonWebTokenError} from "jsonwebtoken";
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import 
-const userSchema=new Schema(
-    {
-        username:{
-            type:String,
-            required:true,
-            unique: true,
-            lowercase:true,
-            trim:true,
-            index:true
-        },
-        email:{
-            type:String,
-            required:true,
-            unique: true,
-            lowercase:true,
-            trim:true,
-        },
-        fullname:{
-            type:String,
-            required:true,
-            trim:true,
-            index:true
-        },
-        avatrar:{
-            type: String,//it is going to use a cloudanaity url,
-            required: true,
-        },
-        coverimage:{
-            type: String,//it is going to use a cloudanaity url,
-        },
-        watchHistory:[
-            {
-                type:Schema.Types.ObjectId,
-                ref:"Video"
-            }
-        ],
-        password:{
-            type:String,
-            required:[true,'Password is required']
-        },
-        refreshToken:{
-            type:String
-        }
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // Cloudinary URL
+      required: true,
+    },
+    coverImage: {
+      type: String, // Cloudinary URL
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+
+// Compare Password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+
+// Generate Access Token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
     {
-        timestamps:true
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-)
-export const User=mongoose.model("User",userSchema)
-userSchema.pre("save",async function(next) {
-    if(!this.isModified("password"))return next();
-    this.password=bcrypt.hash(this.password,10)
-    next()
-})
-userSchema.methods.isPasswordCorrect=async function(password){
-    return await bcrypt.compare(password,this.password)
-}
-userSchema.methods.generateAccessToken=function(){
-    JsonWebTokenError.sign(
-        {
-            _id:this._id,
-            email:this.email,
-            username:this.username,
-            fullName:this.fullname
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
-        }
-    )
-}
-userSchema.methods.generateAccessToken=function(){
-    JsonWebTokenError.sign(
-        {
-            _id:this._id,
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-        }
-    )
-}
+  );
+};
+
+
+// Generate Refresh Token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+
+export const User = mongoose.model("User", userSchema);
